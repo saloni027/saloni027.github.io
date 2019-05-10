1cd76249d810f235fd1818ed393aaeee1efadee0
Saloni Sinha <salonisinha068@gmail.com>
2019-05-10 15:06:41 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+    $('.menu .item').tab();
+
+/* this should change both tab content and tab item */
+$.tab('change tab', 'second');
+//    $('.menu .item[data-tab="projects"]').addClass("active");
+
+
+});
+
+////$('.ui.menu').find('.item').tab('change tab', 'home')
+////
+////$('.menu .item[data-tab="home"]').removeClass("active");
+////$('.menu .item[data-tab="projects"]').addClass("active");
+////$('.menu .item[data-tab="projects"]').removeClass("active");
+////$('.menu .item[data-tab="contacts"]').addClass("active");
+//
+//
+//$('.menu .item').tab();
+
